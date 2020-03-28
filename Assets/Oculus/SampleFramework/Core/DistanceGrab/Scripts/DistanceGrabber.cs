743bdd14e6ad3f3921d91ad3e74c3774fdb2d16e
Samuel Brache <smbrache@gmail.com>
2020-03-28 14:31:03 -0700

    added handgun crosshair and edited grab point

--- Assets/Oculus/SampleFramework/Core/DistanceGrab/Scripts/DistanceGrabber.cs
+++ Assets/Oculus/SampleFramework/Core/DistanceGrab/Scripts/DistanceGrabber.cs
@@ -198,6 +198,21 @@ namespace OculusSampleFramework
                 return;
             }
 
+            // Set up offsets for grabbed object desired position relative to hand.
+            m_grabbedObjectPosOff = m_gripTransform.localPosition;
+            if (m_grabbedObj.snapOffset)
+            {
+                Vector3 snapOffset = m_grabbedObj.snapOffset.position;
+                if (m_controller == OVRInput.Controller.LTouch) snapOffset.x = -snapOffset.x;
+                m_grabbedObjectPosOff += snapOffset;
+            }
+
+            m_grabbedObjectRotOff = m_gripTransform.localRotation;
+            if (m_grabbedObj.snapOffset)
+            {
+                m_grabbedObjectRotOff = m_grabbedObj.snapOffset.rotation * m_grabbedObjectRotOff;
+            }
+
             Rigidbody grabbedRigidbody = m_grabbedObj.grabbedRigidbody;
             Vector3 grabbablePosition = pos + rot * m_grabbedObjectPosOff;
             Quaternion grabbableRotation = rot * m_grabbedObjectRotOff;
